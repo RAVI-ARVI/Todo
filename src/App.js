dfa612b15ceb4c095f2ad70f2e711785ee6b46b1
ravi <ravi8688@gmail.com>
2022-06-22 19:00:42 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+import Testtodo from './component/todoProject/todo'
+
+const App = () => {
+  return (
+    <>
+    <Testtodo/>
+    
+     
+    </>
+  )
+}
+
+export default App
